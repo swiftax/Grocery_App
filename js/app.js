f197a150d21bfa7e7eef6b05a81dcc781ae532fd
Anthony Tropeano <tony@swiftax.com>
2015-05-20 14:36:38 -0400

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,35 @@
-$('ul').on('click', 'li', function(event){
-  // fires when any LIs are clicked on
-  // including LIs that aren't on the page when it is initially loaded
+
+//To remove items
+
+$(':header').addClass('headLine');
+$('.app > li:lt(4)').hide().fadeIn(1500);
+$('li').on('click', function(){
+	$(this).remove();
+});
+
+
+// To Show and Hide button and form
+
+$(function(){
+	var $newItemButton = $('#newItemButton');
+	var $newItemForm = $('#newItemForm');
+	var $textInput = $('input:text');
+
+	$newItemButton.show();
+	$newItemForm.hide();
+
+	$('#showForm').on('click' , function(){
+		$newItemButton.hide();
+		$newItemForm.show();
+	});
+
+  $newItemForm.on('submit', function(e){
+    e.preventDefault();
+    var newText = $textInput.val();
+    $('li:last').after('<li>' + newText + '</li>');
+    $newItemForm.hide();
+    $newItemButton.show();
+    $textInput.val('');
+  });
+
 });
